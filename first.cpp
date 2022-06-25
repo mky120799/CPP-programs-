c00565b21abb69b56027e44cf05e62c586571651
Mithilesh Kumar Yadav <mky120799@gmail.com>
2022-06-25 14:30:07 +0530

    first commit

---
+++ first.cpp
@@ -0,0 +1,20 @@
+// Simple C++ program to display "Hello World"
+
+// Header file for input output functions
+#include<iostream>
+#include<conio.h>
+#include<stdlib.h>
+
+using namespace std;
+
+// main function -
+// where the execution of program begins
+int main()
+{
+	
+
+	// prints hello world
+	cout<<" i am very happy at the end this is workin";
+	
+	return 0;
+}
